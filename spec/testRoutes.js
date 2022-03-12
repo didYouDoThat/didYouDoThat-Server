75e3621435ca80cac0633359ddfdddfc9b6b38a4
okcorn <jsok79now@gmail.com>
2022-03-12 19:42:22 +0000

    test: add delete, get habitList test

--- spec/testRoutes.js
+++ spec/testRoutes.js
@@ -8,7 +8,7 @@ const User = require("../models/User");
 const Habit = require("../models/Habit");
 const CatImage = require("../models/CatImage");
 
-describe("POST /users/:userId/habit", function () {
+describe("Route Test", function () {
   this.timeout(10000);
 
   const mongoose = require("mongoose");
@@ -31,7 +31,7 @@ describe("POST /users/:userId/habit", function () {
     })();
   });
 
-  describe("Setting for test", () => {
+  describe("Setting up for tests", () => {
     let newUser;
     let newUserId;
     let newUserToken;
@@ -59,6 +59,16 @@ describe("POST /users/:userId/habit", function () {
       localTimeOffset: 9,
     };
 
+    const mockHabit2 = {
+      title: "test2",
+      localTimeOffset: 9,
+    };
+
+    const updateInfo = {
+      currentLocalDate: new Date().toISOString,
+      localTimeOffset: 9,
+    };
+
     it("Make new habit", (done) => {
       request(app)
         .post(`/users/${newUserId}/habit`)
@@ -74,11 +84,65 @@ describe("POST /users/:userId/habit", function () {
           const newHabit = res.body.newHabit;
 
           expect(newHabit).to.exist();
-
           expect(newHabit.title).to.be("test");
           expect(newHabit.catImage).to.exist();
         });
 
+      done();
+    });
+
+    it("Get habit", (done) => {
+      request(app)
+        .get(`/users/${newUserId}`)
+        .set("authorization", `Bearer ${newUserToken}`)
+        .type("application/json")
+        .expect(200)
+        .end(async (err, res) => {
+          if (err) {
+            return done(err);
+          }
+
+          const targetHabitList = res.body.habitList;
+
+          expect(targetHabitList).to.exist();
+          expect(targetHabitList.length).to.be(1);
+          expect(targetHabitList[0].title).to.be("test");
+          expect(targetHabitList[0].status).to.be(0);
+        });
+      done();
+    });
+
+    it("Delete habit", (done) => {
+      request(app)
+        .post(`/users/${newUserId}/habit`)
+        .set("authorization", `Bearer ${newUserToken}`)
+        .type("application/json")
+        .send(mockHabit2)
+        .expect(200)
+        .end(async (err, res) => {
+          if (err) return done(err);
+
+          const habitList = await User.findById(newUserId).lean().exec();
+          const targetHabitId = habitList[0]._id;
+
+          expect(habitList.length).to.be(2);
+
+          request(app)
+            .delete(`/users/${newUserId}/habits/${targetHabitId}`)
+            .type("application/json")
+            .expect(200)
+            .end(async (err, res) => {
+              if (err) return done(err);
+
+              const updatedHabitList = await User.findById(newUserId)
+                .lean()
+                .exec();
+
+              expect(res.body.result).to.be("success");
+              expect(updatedHabitList.length).to.be(1);
+              expect(updatedHabitList[0].title).to.be(mockHabit2.title);
+            });
+        });
       done();
     });
   });
